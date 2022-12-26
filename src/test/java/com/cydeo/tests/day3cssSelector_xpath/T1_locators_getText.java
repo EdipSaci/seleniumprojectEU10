dab7a7c33e5c99eefdafc580d33367ffd2f501b5
Edip SACİ <saciedip@gmail.com>
2022-12-26 11:31:43 +0300

    Selenium day3 review task done

---
+++ src/test/java/com/cydeo/tests/day3cssSelector_xpath/T1_locators_getText.java
@@ -0,0 +1,57 @@
+package com.cydeo.tests.day3cssSelector_xpath;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T1_locators_getText {
+
+    public static void main(String[] args) {
+
+        //1- Open a chrome browser
+        //WebDriverManager.chromedriver().setup();
+        //WebDriver driver = new ChromeDriver();
+        WebDriver driver= new WebDriverFactory().getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2- Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+        //3- Enter incorrect username: “incorrect”
+        WebElement userName= driver.findElement(By.name("USER_LOGIN"));
+        userName.sendKeys("incorrect");
+
+        //4- Enter incorrect password: “incorrect”
+        WebElement password= driver.findElement(By.name("USER_PASSWORD"));
+        password.sendKeys("incorrect");
+
+        //5- Click to login button.
+        WebElement loginButton= driver.findElement(By.className("login-btn"));
+        loginButton.click();
+
+        //6- Verify error message text is as expected:
+        WebElement errorMessage= driver.findElement(By.className("errortext"));
+        System.out.println(errorMessage.getText());
+        String exceptedErrorMessage= "Incorrect login or password";
+        String actualErrorMessage= errorMessage.getText();
+
+        if (actualErrorMessage.equals(exceptedErrorMessage)){
+            System.out.println("Error message verification is PASSED!");
+        }else {
+            System.out.println("Error message verification is FAİLED!");
+        }
+    }
+}
+/*
+TC #1: NextBaseCRM, locators and getText() practice
+1- Open a chrome browser
+2- Go to: https://login1.nextbasecrm.com/
+3- Enter incorrect username: “incorrect”
+4- Enter incorrect password: “incorrect”
+5- Click to login button.
+6- Verify error message text is as expected:
+Expected: Incorrect login or password
+PS: Inspect and decide which locator you should be using to locate web
+elements.
+ */
