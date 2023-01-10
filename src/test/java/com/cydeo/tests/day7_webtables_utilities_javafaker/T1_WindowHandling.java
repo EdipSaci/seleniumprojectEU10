e7767bc1acd686c8a1eb29a1d9d7b7399bb272c2
Edip SACİ <saciedip@gmail.com>
2023-01-10 19:26:23 +0300

    day7 T1_WindowHandling redone with BrowserUtils

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -1,13 +1,12 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class T1_WindowHandling {
@@ -32,6 +31,8 @@ public class T1_WindowHandling {
         ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
 
         //4. Create a logic to switch to the tab where Etsy.com is open
+
+        /*
         Set<String> allWindowHandles = driver.getWindowHandles();
 
         for (String each : allWindowHandles ) {
@@ -50,6 +51,10 @@ public class T1_WindowHandling {
         String exceptedTitle= "Etsy";
 
         Assert.assertTrue(actualTitle.contains(exceptedTitle));
+
+         */
+
+        BrowserUtils.switchWindowAndVerify(driver,"etsy","Etsy");
 
     }
 }
---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,64 @@
+package com.cydeo.utilities;
+
+    /*
+    In this class only general utility methods that are NOT related to some specific page.
+     */
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
+public class BrowserUtils {
+
+    /*
+   This method will accept int (in seconds) and execute Thread.sleep for given duration
+    */
+    public static void sleep(int second){
+        second*=1000;
+        try {
+            Thread.sleep(second);
+        }catch (InterruptedException e){
+
+        }
+    }
+
+
+
+    /*
+    This method accepts 3 arguments.
+    Arg1: webdriver
+    Arg2: expectedInUrl : for verify if the url contains given String.
+        - If condition matches, will break loop.
+    Arg3: expectedInTitle to be compared against actualTitle
+     */
+
+    public static void switchWindowAndVerify(WebDriver driver,String exceptedInUrl,String expectedInTitle){
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+        for (String each : allWindowHandles ) {
+
+            driver.switchTo().window(each);
+
+            System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(exceptedInUrl)){
+                break;
+            }
+        }
+        //5. Assert: Title contains "expectedInTitle"
+        String actualTitle= driver.getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+    }
+
+   /*
+    This method accepts a String "expectedTitle" and Asserts if it is true
+     */
+
+    public static void verifyTitle(WebDriver driver,String exceptedTitle){
+        Assert.assertEquals(driver.getTitle(),exceptedTitle);
+    }
+
+
+}
